9a261e3516a8f5f90bd75648de94216ae34cf074
Ven <ca.jmj@outlook.com>
2023-08-05 00:10:59 -0400

    Login Validation for errors

--- users/views.py
+++ users/views.py
@@ -1,11 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from .forms import UserRegistrationForm
 
 # Create your views here.
 
 def register(request):
     if request.method == 'POST':
-        pass
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('home')
+        else:
+            messages.error(request, f'Error!')
+            return render(request, 'users/register.html', {'form': form})
+
     else:
         form = UserRegistrationForm()
         return render(request, 'users/register.html', {'form': form})
+    
+
+def profile(request):
+    return render(request, 'dashboard/about.html')
