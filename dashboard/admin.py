894f29a1d5678316695518fcae4153eb4caff1e2
Ven <ca.jmj@outlook.com>
2023-09-02 23:10:51 -0400

    Media & Profile Pic UPLOAD

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Company
+from .models import Company, Project, Task
 
 
 # Register your models here.
 admin.site.register(Company)
+admin.site.register(Project)
+admin.site.register(Task)
